b7cd74ca0bccb4104188ab583492b1500b856bed
rico3d <rico3d2001@gmmail.com>
2015-05-19 00:18:54 -0300

    Innitial commit

---
+++ Jonia.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jonia.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestJonia
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
