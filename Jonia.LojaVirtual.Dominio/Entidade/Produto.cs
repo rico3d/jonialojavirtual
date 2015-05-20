283294ec234abc07359017c694c624d6971c7302
rico3d <rico3d2001@gmmail.com>
2015-05-20 19:08:07 -0300

    Commit20-05-01

--- Jonia.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Jonia.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -10,6 +10,7 @@ namespace Jonia.LojaVirtual.Dominio.Entidade
     {
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
+        public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string  Categoria { get; set; }
 
