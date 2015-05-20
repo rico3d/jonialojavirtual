13bbd22210078cbf0cff8782d548cf89ca2462ea
rico3d <rico3d2001@gmmail.com>
2015-05-20 20:01:35 -0300

    Aula 6

---
+++ Jonia.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jonia.LojaVirtual.Dominio.Entidade;
+
+namespace Jonia.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext:DbContext
+    {
+        public DbSet<Produto> Produtos { get; set;}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ Jonia.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jonia.LojaVirtual.Dominio.Repositorio;
+
+namespace Jonia.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+
+        //
+        // GET: /Produtos/
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+            return View(produtos);
+        }
+	}
+}
